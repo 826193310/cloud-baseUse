2dbf6a0380299399278710cf564cb973df9ed98f
苏观洲 <guanzhou.su@inin88.com>
2020-09-05 15:51:34 +0800

    Feat: gateway 网关服务初始化， 注意 pom 里面不要增加 web 和 actuator 的依赖

---
+++ cloud-gateway-gateway9527/src/main/java/com/su/springcloud/GatewayMain9527.java
@@ -0,0 +1,19 @@
+package com.su.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Classname GatewayMain9527
+ * @Description gateway 微服务演示
+ * @Date 2020/9/5 15:27
+ * @Created by SGZ
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
