cfae51cf18f68c92e2f82f9306ed7690a66ec78b
苏观洲 <guanzhou.su@inin88.com>
2020-11-07 11:02:33 +0800

    Feat: 114 至 123 流量控制和服务降级

--- cloudalibaba-sentinel-service8401/src/main/java/com/su/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/su/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.su.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Classname FlowLimitController
@@ -23,6 +26,7 @@ public class FlowLimitController {
     **/
     @GetMapping("/testA")
     public String testA() {
+        //System.out.println("testA");
         return "----testA";
     }
 
@@ -38,5 +42,67 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB() {
         return "----testB";
+    }
+
+    /**
+     *
+     *@Description: 测试降级规则 http://localhost:8401/testC
+     *@param: null
+     *@Author: SGZ
+     *@Date: 2020/11/3
+     *@return:
+     *
+     **/
+    @GetMapping("/testC")
+    public String testC() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(300);
+        return "----testC";
+    }
+
+    /**
+     *
+     *@Description: 测试降级规则 http://localhost:8401/testD
+     *@param: null
+     *@Author: SGZ
+     *@Date: 2020/11/3
+     *@return:
+     *
+     **/
+    @GetMapping("/testD")
+    @SentinelResource(value = "testD")
+    public String testD() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(300);
+        return "----testD";
+    }
+
+    /**
+     *
+     *@Description: 测试服务降级之异常比例 http://localhost:8401/fwjj/ycbl
+     *@param: null
+     *@Author: SGZ
+     *@Date: 2020/11/3
+     *@return:
+     *
+     **/
+    @GetMapping("/fwjj/ycbl")
+    public String ycbl() {
+        int a = 10/0;
+        return "----ycbl";
+    }
+
+    /**
+     *
+     *@Description: 测试服务降级之异常数 http://localhost:8401/fwjj/ycs
+     * 如果一分钟之内，异常数超过配置的异常数，时间窗口要大于等于60秒，否则可能造成一直都是熔断状态的处理
+     *@param: null
+     *@Author: SGZ
+     *@Date: 2020/11/3
+     *@return:
+     *
+     **/
+    @GetMapping("/fwjj/ycs")
+    public String ycs() {
+        int a = 10/0;
+        return "----ycs";
     }
 }
