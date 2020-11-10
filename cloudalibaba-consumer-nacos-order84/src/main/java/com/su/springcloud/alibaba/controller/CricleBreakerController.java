1f9826a0f434485339ecd232337ceaeb8a684dc9
苏观洲 <guanzhou.su@inin88.com>
2020-11-10 17:38:00 +0800

    Feat: 132_Sentinel服务熔断只配置fallback

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/su/springcloud/alibaba/controller/CricleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/su/springcloud/alibaba/controller/CricleBreakerController.java
@@ -33,7 +33,8 @@ public class CricleBreakerController {
     *
     **/
     @RequestMapping("/consumer/fallback/{id}")
-    @SentinelResource(value = "fallback") // 没有配置异常捕获和限流自定义处理
+    //@SentinelResource(value = "fallback") // 没有配置异常捕获和限流自定义处理
+    @SentinelResource(value = "fallback", fallback = "handlerFallback") // fallback 只负责业务异常
     public CommonResult<Payment> fallback(@PathVariable("id") Long id) {
         CommonResult<Payment> result = restTemplate.getForObject(SERVER_URL + "/paymentSQL/" + id, CommonResult.class, id);
 
@@ -43,5 +44,10 @@ public class CricleBreakerController {
             throw new NullPointerException("NullPointerException, 该ID没有对应记录，空指针异常");
         }
         return result;
+    }
+
+    public CommonResult handlerFallback(@PathVariable("id") Long id, Throwable e) {
+        Payment payment = new Payment(id, null);
+        return new CommonResult(888, "兜底异常 handlerFallback, exception内容:" + e.getMessage(), payment);
     }
 }
